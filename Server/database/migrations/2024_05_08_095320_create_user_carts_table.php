80283479f6758b0cfe25e3547d77cf0c8fb18530
OCHIENGHerman <ochiengherman32@gmail.com>
2024-05-08 13:34:22 +0300

    add product and user reference

--- Server/database/migrations/2024_05_08_095320_create_user_carts_table.php
+++ Server/database/migrations/2024_05_08_095320_create_user_carts_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_carts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
             $table->integer('quantity')->default(1); 
             $table->timestamps();
