8ef63e67626025d56f271285508dafb2b382122d
OCHIENGHerman <ochiengherman32@gmail.com>
2024-05-08 13:50:36 +0300

    add routes

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\SelectedProductsController;
 use App\Http\Controllers\UserController;
+use App\Htt\Controllers\UserCartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -45,6 +46,16 @@ Route::group([
     
     
 });
+
+// Get all users
+Route::get('/users', [UserController::class, 'getAllUsers']);
+
+// Cart Routes
+Route::get('/users/{userId}/cart', [UserCartController::class, 'getCartItemsByUserId']);
+// Route::get('/carts', [CartController::class, 'getAllCartItems']);
+Route::post('/carts', [UserCartController::class, 'addItemToCart']);
+// Route::put('/carts/{id}', [CartController::class, 'editCartItem']);
+// Route::delete('/carts/{id}', [CartController::class, 'deleteCartItem']);
 
 
 Route::group([
