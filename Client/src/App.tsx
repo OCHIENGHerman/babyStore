4cef7cae60210a5074921550842b9abef7113b93
OCHIENGHerman <ochiengherman32@gmail.com>
2024-05-28 17:36:39 +0300

    start frontend implementation

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,43 +1,12 @@
-import { Routes, Route, } from 'react-router-dom'
-import Homepage from './pages/homepage'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Cart from './pages/Cart'
-import ProductDetails from './pages/productDetails'
-import LayoutAdmin from './Components/Admin/Shared/LayoutAdmin'
-import AdminDashboard from './Components/Admin/AdminDashboard'
-import AdminOrder from './Components/Admin/AdminOrder'
-import AdminProducts from './Components/Admin/AdminProducts'
-import AdminCustomers from './Components/Admin/AdminCustomers'
-import UserDetails from './Components/Admin/UserDetails'
-import AddProduct from './Components/Admin/AddProduct'
-import UpdateProduct from './Components/Admin/UpdateProduct'
-import useAddToCart from './hooks/useAddToCart'
-
-
+import './App.css'
 
 function App() {
-  const { cartItems, addToCart, cartCount } = useAddToCart();
+  
 
-  return ( 
-    <Routes>
-    <Route path="/" element={<Homepage addToCart={addToCart}  cartItems={cartItems} cartCount={cartCount} />} />
-    <Route path='/login' element={<Login />}/>
-    <Route path='/register' element={<Register />}/>
-    <Route path="/cart" element={<Cart cartItems={cartItems} />} />
-    <Route path='/admin' element={<LayoutAdmin />}>
-      <Route index element={<AdminDashboard />}/>
-      <Route path='/admin/orders' element={<AdminOrder />}/>
-      <Route path='/admin/products' element={<AdminProducts />}/>
-      <Route path='/admin/customers' element={<AdminCustomers />}/>
-      <Route path='/admin/userdetails/:id' element={<UserDetails />}/>
-      <Route path='/admin/addproduct' element={<AddProduct />}/>
-      <Route path='/admin/updateproduct/:id' element={<UpdateProduct />}/>
-    </Route>
-
-    <Route path='/product/:id' element={<ProductDetails />}/>
-  </Routes>
-    
+  return (
+    <>
+      <h1>New App</h1>
+    </>
   )
 }
 
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,21 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
-import { ThemeProvider } from './context/themedContext.tsx'
-import { UserProvider } from './context/userContext.tsx'
-import { ProductProvider } from './context/productContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <UserProvider>
-        <ProductProvider>
-          <App />
-        </ProductProvider>
-      </UserProvider>
-    </ThemeProvider>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
-
-
