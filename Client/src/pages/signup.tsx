e8b9e098ff1bdfdf64571a59475bf7d5e01d83b9
OCHIENGHerman <ochiengherman32@gmail.com>
2024-05-30 15:32:08 +0300

    achieve loading state

--- Client/src/pages/signup.tsx
+++ Client/src/pages/signup.tsx
@@ -21,11 +21,19 @@ const Signup: React.FC = () => {
         password_confirmation: '',
         phone_number: '',
     });
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [message, setMessage] = useState<string | null>(null);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setFormData({ ...formData, [e.target.name]: e.target.value});
     };
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setLoading(true);
+        setError(null);
+        setMessage(null);
 
         try {
             const response = await axios.post('http://127.0.0.1:8000/api/register-normal-user', formData, {
@@ -34,17 +42,26 @@ const Signup: React.FC = () => {
                 }
             });
             if (response.data.status) {
-                console.log(response.data.message);
+                // console.log(response.data.message);
+                setMessage(response.data.message);
                 navigate('/login');
             } else {
-                console.error(response.data.message);
+                // console.error(response.data.message);
+                setError(response.data.error);
             }
         } catch (error) {
-            console.error(error);
+            setError('An error occurred. Please try again later.');
+            // console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
+        <div>
+         {loading && <p>Loading...</p>}
+         {error && <p>{error}</p>}
+         {message && <p>{message}</p>} 
         <form onSubmit={handleSubmit}>
             <input
                 type='text'
@@ -88,8 +105,11 @@ const Signup: React.FC = () => {
                 value={formData.phone_number}
                 onChange={handleChange}
             />
-            <button type='submit'>Register</button>
+            <button type='submit' disabled={loading}>
+                {loading ? 'Registering...' : 'Register'}
+            </button>
         </form>
+        </div>
     );
 };
 
