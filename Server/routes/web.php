2e201758544b98f5919e86e7d748064e1109bdfd
OCHIENGHerman <ochiengherman32@gmail.com>
2024-06-22 17:53:11 +0300

    test web url

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Handling unauthenticated routes // Testing
+Route::get( '/unauthenticated', [AuthController::class, 'unauthenticated'])->name('login');
